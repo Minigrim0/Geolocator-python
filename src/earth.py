652574a110e9e9255c097d81c294ac6fb399aacf
Minigrim0 <grimauflorent@gmail.com>
2023-05-21 12:18:39 +0200

    [Add] Earth coords

--- src/earth.py
+++ src/earth.py
@@ -1,3 +1,25 @@
+import pygame as pg
+from src.utils import get_decimal_from_dms, get_dms_from_decimal
+
 class Earth:
     def __init__(self):
-        pass
+        self.image = pg.image.load("assets/earth.jpg")
+        self.rect = pg.Rect((0, 0), (1400, 700))
+
+    def draw(self, window):
+        window.blit(pg.transform.scale(self.image, (1400, 700)), (0, 0))
+
+    def get_coord_from_mouse(self, mouse_pos):
+        return (
+            180 - (mouse_pos[0] / 1400 * 360),
+            90 - (mouse_pos[1] / 700 * 180)
+        )
+
+    def OnEvent(self, event):
+        if event.type == pg.MOUSEMOTION:
+            coord = self.get_coord_from_mouse(pg.mouse.get_pos())
+            current_coord = (
+                "{}d{}m{}s".format(*get_dms_from_decimal(coord[0])),
+                "{}d{}m{}s".format(*get_dms_from_decimal(coord[1]))
+            )
+            print(f"Current coord: {current_coord}")
---
+++ src/utils.py
@@ -0,0 +1,28 @@
+def get_decimal_from_dms(dms, ref):
+
+    degrees = dms[0][0] / dms[0][1]
+    minutes = dms[1][0] / dms[1][1] / 60.0
+    seconds = dms[2][0] / dms[2][1] / 3600.0
+
+    if ref in ['S', 'W']:
+        degrees = -degrees
+        minutes = -minutes
+        seconds = -seconds
+
+    return round(degrees + minutes + seconds, 5)
+
+
+def get_dms_from_decimal(decimal):
+    degrees = int(decimal)
+    minutes = int((decimal - degrees) * 60)
+    seconds = (decimal - degrees - minutes / 60) * 3600.00
+
+    return degrees, minutes, int(seconds * 100)
+
+
+def get_coordinates(geotags):
+    lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
+
+    lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
+
+    return (lat,lon)

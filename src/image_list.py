c1eec59e5fb6015a0b0713d610031b5eaba4b84f
Minigrim0 <grimauflorent@gmail.com>
2023-05-21 12:18:46 +0200

    [Upd] Eventhandling

--- src/image_list.py
+++ src/image_list.py
@@ -4,6 +4,8 @@ import pygame as pg
 from src.runnable import Runnable
 from src.image_vignette import ImageVignette
 from src.loading_screen import loading_screen
+from src.earth import Earth
+
 
 class ImageList(Runnable):
     def __init__(self, dir):
@@ -11,6 +13,8 @@ class ImageList(Runnable):
         self.dir = dir
         self.images = []
         self.offset = 0
+        self.rect = pg.Rect((1400, 0), (1900, 1080))
+        self.earth = Earth()
 
     def pre_load(self):
         image_list = sorted(glob.glob(self.dir + "/*.jpg"))
@@ -19,7 +23,7 @@ class ImageList(Runnable):
             self.images.append(ImageVignette(image_path))
 
     def update(self, event):
-        if event.type == pg.MOUSEBUTTONDOWN:
+        if event.type == pg.MOUSEBUTTONDOWN and self.rect.collidepoint(event.pos):
             if event.button == 4:
                 self.offset += 15
                 if self.offset > 0:
@@ -28,7 +32,16 @@ class ImageList(Runnable):
                 self.offset -= 15
                 if self.offset < -len(self.images) * 150 + self.window.get_height():
                     self.offset = -len(self.images) * 150 + self.window.get_height()
+            elif event.button == 1:
+                for image in self.images:
+                    was_selected = image.selected
+                    image.selected = False
+                    if image.rect.collidepoint(event.pos):
+                        image.selected = not was_selected
+        elif self.earth.rect.collidepoint(pg.mouse.get_pos()):
+            self.earth.OnEvent(event)
 
     def draw(self):
+        self.earth.draw(self.window)
         for i, image in enumerate(self.images):
-            image.draw(self.window, (20, self.offset + i * 150 + 20))
+            image.draw(self.window, (1400, self.offset + i * 150 + 20))
